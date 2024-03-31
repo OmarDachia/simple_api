662fcd5e14ea24dd575deb6ef8caa962a0e7580f
Omar Dachia <omardachia@gmail.com>
2024-03-31 08:28:49 +0100

    PHP API Implementation SOurce code

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+// Database configuration
+$hostname = 'localhost';
+$username = "root";
+$password = '';
+$database = 'hr';
+
+// Establish database connection
+$conn = mysqli_connect($hostname, $username, $password, $database);
+
+// Check connection
+if (!$conn) {
+    die('Connection failed: ' . mysqli_connect_error());
+}
+?>
